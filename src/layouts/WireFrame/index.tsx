e4ed47442f91ccb6b854bd671abe8120dccae082
황진욱 <40532902+RoaZium@users.noreply.github.com>
2023-02-21 09:55:05 +0900

    SideBar 메뉴 작업

--- src/layouts/WireFrame/index.tsx
+++ src/layouts/WireFrame/index.tsx
@@ -3,8 +3,9 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 const divStyle = {
   container: {
-    margin: "10px",
-    height: "calc(100vh - 20px)", // 브라우저 높이에서 margin 값만큼 뺀 값
+    // margin: "10px",
+    // height: "calc(100vh - 20px)", // 브라우저 높이에서 margin 값만큼 뺀 값
+    height: "100vh",
     overflow: "auto", // 스크롤 없애기
     background: "skyblue",
   },
--- src/layouts/template01/index.tsx
+++ src/layouts/template01/index.tsx
@@ -1,22 +1,9 @@
 import React from "react";
 import BaseWireFrame from "../WireFrame";
-import { Button, Paper, Box } from "@mui/material";
-import SideBar from "../../components/SideBar";
+import { Button, Box } from "@mui/material";
+import DrawerBase from "../../components/Drawer";
+
 import "../../styles/Base/Base.css";
-
-import { Drawer } from "@mui/material";
-import Divider from "@mui/material/Divider";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import Toolbar from "@mui/material/Toolbar";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import { WorkList } from "../../constants/Works";
-
-const drawerWidth = 220;
 
 export default function Template01(props: any) {
   return (
@@ -31,35 +18,13 @@ export default function Template01(props: any) {
               justifyContent: "center",
             }}
           >
-            <Box sx={{ backgroundColor: "lightblue", height: "100%", margin: "10px" }}>
-              <Drawer
-                sx={{
-                  color: "blue",
-                  width: drawerWidth,
-                  flexShrink: 0,
-                  "& .MuiDrawer-paper": {
-                    width: drawerWidth,
-                    boxSizing: "border-box",
-                  },
-                }}
-                variant="permanent"
-                anchor="left"
-              >
-                <Toolbar />
-                <Divider />
-                <List>
-                  {WorkList.map((text, index) => (
-                    <ListItem key={text} disablePadding>
-                      <ListItemButton>
-                        <ListItemIcon>
-                          {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                        </ListItemIcon>
-                        <ListItemText primary={text} />
-                      </ListItemButton>
-                    </ListItem>
-                  ))}
-                </List>
-              </Drawer>
+            <Box
+              sx={{
+                backgroundColor: "lightblue",
+                height: "100%",
+              }}
+            >
+              <DrawerBase />
             </Box>
           </div>
           <div
