33fbecaeab7f5a8298af539f73df2d70af45a197
황진욱 <40532902+RoaZium@users.noreply.github.com>
2023-03-15 21:28:32 +0900

    LayoutContainer 구조 작업

--- src/components/LayoutContainers/LS-H-C-Container/index.jsx
+++ src/components/LayoutContainers/LS-H-C-Container/index.jsx
@@ -4,13 +4,13 @@ import SDHeader from "../../../components/SDHeader/SDHeader";
 import { SDFullSideBar } from "../../../components/SDSideBar/SDSideBar";
 import SDMain from "../../../components/SDMain/SDMain";
 
-const LSHCContainer = () => {
+const LSHCContainer = ({ sidebar, header, content }) => {
   return (
     <div className="container">
-      <div className="nav-full">SideBar</div>
+      <div className="nav-full">{sidebar}</div>
       <div className="row-container">
-        <div className="header-content">Header</div>
-        <div className="content-header">Content</div>
+        <div className="header-content">{header}</div>
+        <div className="content-header">{content}</div>
       </div>
     </div>
   );
--- src/layouts/TestLayout.jsx
+++ src/layouts/TestLayout.jsx
@@ -1,7 +1,9 @@
+import { Typography } from "@mui/material";
 import HLSCContainer from "../components/LayoutContainers/H-LS-C-Container";
 import HLSCFContainer from "../components/LayoutContainers/H-LS-C-F-Container";
 import LSHCContainer from "../components/LayoutContainers/LS-H-C-Container";
 import MyFlow from "./myflow/MyFlow";
+import SideBar from "../components/SideBar/SideBar";
 
 function TestLayout() {
   return (
@@ -9,7 +11,11 @@ function TestLayout() {
       {/* <HLSCContainer></HLSCContainer> */}
       {/* <HLSCFContainer></HLSCFContainer> */}
       {/* <MyFlow /> */}
-      <LSHCContainer />
+      <LSHCContainer
+        sidebar={<h3>sidebar</h3>}
+        header={<h3>header</h3>}
+        content={<h3>content</h3>}
+      />
     </>
   );
 }
